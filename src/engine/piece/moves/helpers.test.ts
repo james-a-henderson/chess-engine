d8b030d091dea3c7130a5e7048be8c592a6e9baa
James Henderson <james.alden.henderson@gmail.com>
2025-05-26 11:27:45 -0500

    Added helpers.test.ts
    Moved capture tests from standardMove.test.ts to helpers.test.ts. I
    did this because the capture rules will be the same for each move
    type, and having these tests for every move type is redundant.

---
+++ src/engine/piece/moves/helpers.test.ts
@@ -0,0 +1,327 @@
+import {
+    BoardPosition,
+    CaptureAvailability,
+    GameRules,
+    PieceConfig,
+    PlayerColor,
+    RectangularBoard
+} from '../../../types';
+import { GameEngine } from '../../GameEngine';
+import { Piece } from '../piece';
+import { pieceIsOnPosition, validateCaptureRules } from './helpers';
+
+type testPieceNames = ['foo', 'bar'];
+
+const rulesConfig: GameRules<testPieceNames> = {
+    name: 'test',
+    players: [
+        { color: 'white', order: 0 },
+        { color: 'black', order: 1 }
+    ],
+    board: {
+        height: 8,
+        width: 8
+    },
+    winConditions: [{ condition: 'resign' }],
+    drawConditions: [],
+    pieces: []
+};
+
+describe('helpers', () => {
+    describe('validateCaptureRules', () => {
+        describe('required', () => {
+            test('returns true if piece is black and destination space contains white piece', () => {
+                generateCaptureTest(
+                    'required',
+                    'black',
+                    ['c', 3],
+                    ['c', 5],
+                    true,
+                    true
+                );
+            });
+
+            test('returns false if piece is black and destination space contains no piece', () => {
+                generateCaptureTest(
+                    'required',
+                    'black',
+                    ['c', 3],
+                    ['c', 5],
+                    false,
+                    false
+                );
+            });
+
+            test('returns true if piece is white and destination space contains black piece', () => {
+                generateCaptureTest(
+                    'required',
+                    'white',
+                    ['c', 5],
+                    ['c', 3],
+                    true,
+                    true
+                );
+            });
+
+            test('returns false if piece is white and destination space contains no piece', () => {
+                generateCaptureTest(
+                    'required',
+                    'white',
+                    ['c', 5],
+                    ['c', 3],
+                    false,
+                    false
+                );
+            });
+        });
+
+        describe('optional', () => {
+            test('returns true if piece is black and destination space contains white piece', () => {
+                generateCaptureTest(
+                    'optional',
+                    'black',
+                    ['c', 3],
+                    ['c', 5],
+                    true,
+                    true
+                );
+            });
+
+            test('returns true if piece is black and destination space contains no piece', () => {
+                generateCaptureTest(
+                    'optional',
+                    'black',
+                    ['c', 3],
+                    ['c', 5],
+                    false,
+                    true
+                );
+            });
+
+            test('returns true if piece is white and destination space contains black piece', () => {
+                generateCaptureTest(
+                    'optional',
+                    'white',
+                    ['c', 5],
+                    ['c', 3],
+                    true,
+                    true
+                );
+            });
+
+            test('returns true if piece is white and destination space contains no piece', () => {
+                generateCaptureTest(
+                    'optional',
+                    'white',
+                    ['c', 5],
+                    ['c', 3],
+                    false,
+                    true
+                );
+            });
+        });
+
+        describe('forbidden', () => {
+            test('returns false if piece is black and destination space contains white piece', () => {
+                generateCaptureTest(
+                    'forbidden',
+                    'black',
+                    ['c', 3],
+                    ['c', 5],
+                    true,
+                    false
+                );
+            });
+
+            test('returns true if piece is black and destination space contains no piece', () => {
+                generateCaptureTest(
+                    'forbidden',
+                    'black',
+                    ['c', 3],
+                    ['c', 5],
+                    false,
+                    true
+                );
+            });
+
+            test('returns false if piece is white and destination space contains black piece', () => {
+                generateCaptureTest(
+                    'forbidden',
+                    'white',
+                    ['c', 5],
+                    ['c', 3],
+                    true,
+                    false
+                );
+            });
+
+            test('returns true if piece is white and destination space contains no piece', () => {
+                generateCaptureTest(
+                    'forbidden',
+                    'white',
+                    ['c', 5],
+                    ['c', 3],
+                    false,
+                    true
+                );
+            });
+        });
+    });
+
+    describe('pieceIsOnPosition', () => {
+        test.each([
+            {
+                pieceFile: 'a',
+                pieceRank: 3,
+                inputFile: 'a',
+                inputRank: 3,
+                expected: true
+            },
+            {
+                pieceFile: 'f',
+                pieceRank: 4,
+                inputFile: 'f',
+                inputRank: 4,
+                expected: true
+            },
+            {
+                pieceFile: 'h',
+                pieceRank: 8,
+                inputFile: 'h',
+                inputRank: 8,
+                expected: true
+            },
+            {
+                pieceFile: 'b',
+                pieceRank: 3,
+                inputFile: 'c',
+                inputRank: 3,
+                expected: false
+            },
+            {
+                pieceFile: 'b',
+                pieceRank: 3,
+                inputFile: 'a',
+                inputRank: 3,
+                expected: false
+            },
+            {
+                pieceFile: 'a',
+                pieceRank: 3,
+                inputFile: 'a',
+                inputRank: 2,
+                expected: false
+            },
+            {
+                pieceFile: 'a',
+                pieceRank: 3,
+                inputFile: 'a',
+                inputRank: 4,
+                expected: false
+            },
+            {
+                pieceFile: 'a',
+                pieceRank: 1,
+                inputFile: 'h',
+                inputRank: 8,
+                expected: false
+            }
+        ])(
+            'Piece position: $pieceFile$pieceRank Input Position: $inputFile$inputRank expected: $expected',
+            ({
+                pieceFile,
+                pieceRank,
+                inputFile,
+                inputRank,
+                expected
+            }: {
+                pieceFile: string;
+                pieceRank: number;
+                inputFile: string;
+                inputRank: number;
+                expected: boolean;
+            }) => {
+                const config: PieceConfig<testPieceNames> = {
+                    name: 'foo',
+                    displayCharacters: {
+                        white: 'F',
+                        black: 'f'
+                    },
+                    moves: [],
+                    notation: 'F',
+                    startingPositions: {
+                        white: [[pieceFile, pieceRank]]
+                    }
+                };
+
+                const board: RectangularBoard = {
+                    height: 8,
+                    width: 8
+                };
+                const piece = new Piece(
+                    config,
+                    'white',
+                    [pieceFile, pieceRank],
+                    board
+                );
+
+                const result = pieceIsOnPosition(piece, [inputFile, inputRank]);
+                expect(result).toEqual(expected);
+            }
+        );
+    });
+});
+
+function generateCaptureTest(
+    captureAvailability: CaptureAvailability,
+    playerColor: PlayerColor,
+    startingPosition: BoardPosition,
+    destinationPosition: BoardPosition,
+    pieceOnPosition: boolean,
+    expected: boolean
+) {
+    const captureColor: PlayerColor =
+        playerColor === 'white' ? 'black' : 'white';
+    const pieceConfig: PieceConfig<testPieceNames> = {
+        name: 'foo',
+        notation: 'P',
+        displayCharacters: {
+            white: 'P',
+            black: 'p'
+        },
+        moves: [],
+        startingPositions: {
+            [playerColor]: [startingPosition]
+        }
+    };
+
+    const capturePieceConfig: PieceConfig<testPieceNames> = {
+        name: 'bar',
+        displayCharacters: {
+            white: 'B',
+            black: 'b'
+        },
+        notation: 'B',
+        moves: [],
+        startingPositions: {
+            [captureColor]: [destinationPosition]
+        }
+    };
+    const config: GameRules<testPieceNames> = {
+        ...rulesConfig,
+        pieces: pieceOnPosition
+            ? [pieceConfig, capturePieceConfig]
+            : [pieceConfig]
+    };
+    const engine = new GameEngine(config);
+    const piece = engine.getSpace(startingPosition).piece!;
+
+    const result = validateCaptureRules(
+        piece,
+        engine,
+        destinationPosition,
+        captureAvailability
+    );
+
+    expect(result).toEqual(expected);
+}
