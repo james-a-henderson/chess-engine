dce1f81a734739a1c86d0223557c186180b45f7c
James Henderson <james.alden.henderson@gmail.com>
2025-06-02 10:21:45 -0500

    Fixed typing for _capturedPieces

--- src/engine/GameEngine.ts
+++ src/engine/GameEngine.ts
@@ -19,7 +19,9 @@ export class GameEngine<PieceNames extends string[]> {
     private _players: Player[];
     private _board: RectangularBoard<PieceNames>;
     private _config: GameRules<PieceNames>;
-    private _capturedPieces: Partial<Record<PlayerColor, PieceNames[]>> = {};
+    private _capturedPieces: Partial<
+        Record<PlayerColor, PieceNames[keyof PieceNames][]>
+    > = {};
     private _currentPlayer: PlayerColor;
     private _moves: MoveRecord<PieceNames>[] = [];
 
@@ -166,7 +168,7 @@ export class GameEngine<PieceNames extends string[]> {
         }
 
         this._capturedPieces[space.piece.playerColor]?.push(
-            space.piece.pieceName as PieceNames
+            space.piece.pieceName
         );
 
         space.piece = undefined;
