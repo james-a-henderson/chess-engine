57cd6e053b6bffd981f4c549578974da4f56eb8f
James Henderson <james.alden.henderson@gmail.com>
2025-07-28 12:12:33 -0500

    Refactored otherPieceHasNotMoved to new game state

---
+++ src/engine/piece/moves/restrictions/otherPieceHasNotMovedV2.test.ts
@@ -0,0 +1,144 @@
+import {
+    RectangularBoardConfig,
+    RulesConfigurationError
+} from '../../../../types';
+import { GameStatePiecePlacement } from '../../../gameState';
+import { generateGameState } from '../../../gameState/generateGameState';
+import { generateOtherPieceHasNotMovedFunctionV2 } from './otherPieceHasNotMovedV2';
+
+type testPieceNames = ['foo', 'bar'];
+describe('generateOtherPieceHasNotMovedFunctionV2', () => {
+    const boardConfig: RectangularBoardConfig = {
+        height: 3,
+        width: 3
+    };
+
+    test('returns true if piece is on target space and has not moved', () => {
+        const piecePlacements: GameStatePiecePlacement<testPieceNames>[] = [
+            {
+                piece: { name: 'foo', color: 'white', moveCount: 0 },
+                position: ['a', 1]
+            },
+            {
+                piece: { name: 'bar', color: 'white', moveCount: 0 },
+                position: ['a', 3]
+            }
+        ];
+
+        const state = generateGameState(piecePlacements, 'white', boardConfig);
+        const func = generateOtherPieceHasNotMovedFunctionV2<testPieceNames>(
+            'bar',
+            { white: ['a', 3], black: ['c', 3] }
+        );
+        const result = func(state, ['a', 1]);
+
+        expect(result).toEqual(true);
+    });
+
+    test('returns false if no piece is on target space', () => {
+        const piecePlacements: GameStatePiecePlacement<testPieceNames>[] = [
+            {
+                piece: { name: 'foo', color: 'black', moveCount: 0 },
+                position: ['c', 3]
+            }
+        ];
+
+        const state = generateGameState(piecePlacements, 'black', boardConfig);
+        const func = generateOtherPieceHasNotMovedFunctionV2<testPieceNames>(
+            'bar',
+            { white: ['a', 3], black: ['c', 3] }
+        );
+        const result = func(state, ['a', 1]);
+
+        expect(result).toEqual(false);
+    });
+
+    test('returns false if incorrect piece is on target space', () => {
+        const piecePlacements: GameStatePiecePlacement<testPieceNames>[] = [
+            {
+                piece: { name: 'foo', color: 'white', moveCount: 0 },
+                position: ['a', 1]
+            },
+            {
+                piece: { name: 'foo', color: 'white', moveCount: 0 },
+                position: ['a', 3]
+            }
+        ];
+
+        const state = generateGameState(piecePlacements, 'white', boardConfig);
+        const func = generateOtherPieceHasNotMovedFunctionV2<testPieceNames>(
+            'bar',
+            { white: ['a', 3], black: ['c', 3] }
+        );
+        const result = func(state, ['a', 1]);
+
+        expect(result).toEqual(false);
+    });
+
+    test('returns false if piece of incorrect color is on target space', () => {
+        const piecePlacements: GameStatePiecePlacement<testPieceNames>[] = [
+            {
+                piece: { name: 'foo', color: 'white', moveCount: 0 },
+                position: ['a', 1]
+            },
+            {
+                piece: { name: 'bar', color: 'black', moveCount: 0 },
+                position: ['a', 3]
+            }
+        ];
+
+        const state = generateGameState(piecePlacements, 'white', boardConfig);
+        const func = generateOtherPieceHasNotMovedFunctionV2<testPieceNames>(
+            'bar',
+            { white: ['a', 3], black: ['c', 3] }
+        );
+        const result = func(state, ['a', 1]);
+
+        expect(result).toEqual(false);
+    });
+
+    test('returns false if correct piece is on target space but it has previously moved', () => {
+        const piecePlacements: GameStatePiecePlacement<testPieceNames>[] = [
+            {
+                piece: { name: 'foo', color: 'white', moveCount: 0 },
+                position: ['c', 1]
+            },
+            {
+                piece: { name: 'bar', color: 'white', moveCount: 1 },
+                position: ['c', 3]
+            }
+        ];
+
+        const state = generateGameState(piecePlacements, 'black', boardConfig);
+        const func = generateOtherPieceHasNotMovedFunctionV2<testPieceNames>(
+            'bar',
+            { white: ['a', 3], black: ['c', 3] }
+        );
+        const result = func(state, ['c', 1]);
+
+        expect(result).toEqual(false);
+    });
+
+    test('Throws error if piece color is not in configuration', () => {
+        const piecePlacements: GameStatePiecePlacement<testPieceNames>[] = [
+            {
+                piece: { name: 'foo', color: 'black', moveCount: 0 },
+                position: ['c', 1]
+            },
+            {
+                piece: { name: 'bar', color: 'black', moveCount: 0 },
+                position: ['c', 3]
+            }
+        ];
+
+        const state = generateGameState(piecePlacements, 'black', boardConfig);
+        const func = generateOtherPieceHasNotMovedFunctionV2<testPieceNames>(
+            'bar',
+            { white: ['a', 3] }
+        );
+
+        expect(() => {
+            func(state, ['c', 1]);
+        }).toThrow(RulesConfigurationError);
+    });
+});
---
+++ src/engine/piece/moves/restrictions/otherPieceHasNotMovedV2.ts
@@ -0,0 +1,38 @@
+import {
+    BoardPosition,
+    MoveConditionFunctionV2,
+    PlayerColor,
+    RulesConfigurationError
+} from '../../../../types';
+import { rectangularBoardHelper } from '../../../board';
+import { GameState } from '../../../gameState';
+
+export function generateOtherPieceHasNotMovedFunctionV2<
+    PieceNames extends string[]
+>(
+    pieceName: PieceNames[keyof PieceNames],
+    piecePositionForColor: Partial<Record<PlayerColor, BoardPosition>>
+): MoveConditionFunctionV2<PieceNames> {
+    return (state: GameState<PieceNames>) => {
+        const position = piecePositionForColor[state.currentPlayer];
+
+        if (!position) {
+            throw new RulesConfigurationError(
+                `OtherPieceHasNotMoved improperly configured for player color ${state.currentPlayer}`
+            );
+        }
+
+        const space = rectangularBoardHelper.getSpace(state, position);
+
+        if (
+            !space.piece ||
+            space.piece.color !== state.currentPlayer ||
+            space.piece.name !== pieceName ||
+            space.piece.moveCount > 0
+        ) {
+            return false;
+        }
+
+        return true;
+    };
+}
